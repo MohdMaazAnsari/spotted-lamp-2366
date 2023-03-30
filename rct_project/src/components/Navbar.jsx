bb8036c295d394ae60080934b69293565b886292
maaz <mmaaz7460@gmail.com>
2023-03-30 20:15:12 +0530

    day3

--- rct_project/src/components/Navbar.jsx
+++ rct_project/src/components/Navbar.jsx
@@ -1,13 +1,38 @@
+import {Box,Image,Input,SimpleGrid} from "@chakra-ui/react"
+
+import { PhoneIcon, AddIcon, WarningIcon ,SearchIcon} from '@chakra-ui/icons'
+
 function Navbar(){
 
 
 return(
     <>
-    Navbar</>
+    <SimpleGrid columns={2} spacing={10}>
+
+
+    <Box border="1px solid red">
+
+
+    <Image src="https://cdn.fcglcdn.com/brainbees/images/n/fc_logo.png" alt="logo" />
+
+
+
+
+    </Box>
+
+    <Box>
+        <Input w={500} />
+        <SearchIcon/>
+
+    </Box>
+
+
+    </SimpleGrid>
+
+    
+    
+    </>
 )
-
-
-
 
 
 }
